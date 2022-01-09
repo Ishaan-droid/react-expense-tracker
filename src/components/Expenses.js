427ffba636f1556ee4da26199c2c5afaebfb7666
Ishaan Thakur <ishaant99@gmail.com>
2022-01-08 20:14:51 -0400

    First Commit

---
+++ src/components/Expenses.js
@@ -0,0 +1,32 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        date={props.expenses[0].date}
+        amount={props.expenses[0].amount}
+        title={props.expenses[0].title}
+      />
+      <ExpenseItem
+        date={props.expenses[1].date}
+        amount={props.expenses[1].amount}
+        title={props.expenses[1].title}
+      />
+      <ExpenseItem
+        date={props.expenses[2].date}
+        amount={props.expenses[2].amount}
+        title={props.expenses[2].title}
+      />
+      <ExpenseItem
+        date={props.expenses[3].date}
+        amount={props.expenses[3].amount}
+        title={props.expenses[3].title}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
