6ede2b4fa3b11b151a2c12606d1a47d84b4119e7
Ishaan Thakur <ishaant99@gmail.com>
2022-01-09 11:05:13 -0400

    React state and 2-way binding

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,46 @@
+import React, { useState } from "react";
+
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState();
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        date={props.expenses[0].date}
-        amount={props.expenses[0].amount}
-        title={props.expenses[0].title}
-      />
-      <ExpenseItem
-        date={props.expenses[1].date}
-        amount={props.expenses[1].amount}
-        title={props.expenses[1].title}
-      />
-      <ExpenseItem
-        date={props.expenses[2].date}
-        amount={props.expenses[2].amount}
-        title={props.expenses[2].title}
-      />
-      <ExpenseItem
-        date={props.expenses[3].date}
-        amount={props.expenses[3].amount}
-        title={props.expenses[3].title}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          date={props.expenses[0].date}
+          amount={props.expenses[0].amount}
+          title={props.expenses[0].title}
+        />
+        <ExpenseItem
+          date={props.expenses[1].date}
+          amount={props.expenses[1].amount}
+          title={props.expenses[1].title}
+        />
+        <ExpenseItem
+          date={props.expenses[2].date}
+          amount={props.expenses[2].amount}
+          title={props.expenses[2].title}
+        />
+        <ExpenseItem
+          date={props.expenses[3].date}
+          amount={props.expenses[3].amount}
+          title={props.expenses[3].title}
+        />
+      </Card>
+    </div>
   );
 }
 
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+function ExpenseForm(props) {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+    // setUserInput((prevValue) => {
+    //  return { ...prevValue,
+    //   enteredTitle: e.target.value }
+    // });
+  };
+  const amountChangeHandler = (e) => {
+    setAmount(e.target.value);
+  };
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      title,
+      amount,
+      date: new Date(date),
+    };
+    props.onSaveExpenseData(expenseData);
+
+    setTitle("");
+    setAmount("");
+    setDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input value={title} type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            value={amount}
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={date}
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
